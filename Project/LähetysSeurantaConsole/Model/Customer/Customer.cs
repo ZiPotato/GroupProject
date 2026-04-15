954904c8ae0ef436ccb17247a5f63b63d3fa11cf
Aapeli Juujärvi <ah6911@student.jamk.fi>
2026-04-15 12:56:30 +0300

    Added Id, Email, and PhoneNumber properties with private setters, and implemented a constructor to initialize them with default values.

--- Project/LähetysSeurantaConsole/Model/Customer/Customer.cs
+++ Project/LähetysSeurantaConsole/Model/Customer/Customer.cs
@@ -1,12 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace LähetysSeurantaConsole.Model.Customer
+﻿namespace LähetysSeurantaConsole.Model.Customer
 {
     internal class Customer
     {
         // Here we give the customer a cookie so they can be logged in everytime they enter the site
         // And ofcourse if we do want to look at the "give me your Email" way, I guess it will work.
+
+        public int Id { get; private set; }
+        public string Email { get; private set; }
+        public string PhoneNumber { get; private set; }
+
+        public Customer(int id, string email="", string phoneNumber="")
+        {
+            Id = id;
+            Email = email;
+            PhoneNumber = phoneNumber;
+        }
     }
 }
