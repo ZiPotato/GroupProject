998da7f9cd2841ef772d5ccf9358b7049681bc2b
Vesa-Pekka Tasma <ah8515@student.jamk.fi>
2026-04-17 13:00:25 +0300

    Added an absurd amount of comments now that I am looking back to it. Swapped so the ID is saved as upper already at the constructor and added a "Company" variable that shows the delivery company's name in order to handle the "which constructor do we use for the json" problem.

--- Project/LähetysSeurantaConsole/Model/Package/Package.cs
+++ Project/LähetysSeurantaConsole/Model/Package/Package.cs
@@ -1,4 +1,5 @@
 ﻿using Newtonsoft.Json;
+using System.Reflection;
 
 namespace LähetysSeurantaConsole.Model.Package
 {
@@ -6,49 +7,59 @@ namespace LähetysSeurantaConsole.Model.Package
     {
         private static readonly HttpClient Client = new();
         public string ID { get; set; }
+        public string Company;
         public string Url;
+
+
+        /// <summary>
+        /// Technically we should make it so we parse out the incorrectly formatted information out before this state
+        /// but for the sake of testing and the origin I am doing it this way for the start atleast
+        /// </summary>
         public Package(string iD)
         {
-            ID = iD;
+            ID = iD.ToUpper();    
             UpdateTheParcel();
         }
-        /// <summary>
-        /// Updating the information of the parcel using the tracking number.
-        /// Currently we will likely have to create different handling methods cases for each of the companies, depending on how standardized the URI components are...
-        /// </summary>
 
+        /// <summary>
+        /// Updating the information of the parcel using the tracking identifyer.
+        /// </summary>
         public async Task UpdateTheParcel()
         {
-            if (string.IsNullOrEmpty(Url) && !string.IsNullOrEmpty(ID)) Url = TurningIDToUrl(ID);
-
+            if (string.IsNullOrEmpty(Url) && !string.IsNullOrEmpty(ID)) Url = TurningIDToUrl();
             using HttpResponseMessage response = await Client.GetAsync(Url);
-
             response.EnsureSuccessStatusCode();
-
             string json = await response.Content.ReadAsStringAsync();
+            if (Company == "Matkahuolto")                                   // The naive idea to do either this or the switch way of handling this.
+            {                                                               // We will need to rework this.
+                var result = JsonConvert.DeserializeObject<Parcel>(json);
+            }
 
-            var result = JsonConvert.DeserializeObject<Parcel>(json);
 
         }
-        /// <summary>
-        /// Here we are turning the given ID to an url
+        /// <summary
+        /// This method turns the given tracking identifier into an url
         /// </summary>
-        /// <param name="id">The tracking number</param>
-        /// <returns>The URL for the intended API </returns>
-        /// <exception cref="ArgumentException"></exception>
-        private static string TurningIDToUrl(string id)
+        /// <param name="id"> The tracking identifier </param>
+        /// <returns> The URL for the intended API </returns>
+        /// <exception cref="ArgumentException"> Later in the development we should likely create our own exception class </exception>
+        private string TurningIDToUrl()
         {
-            char[] idarray = id.ToUpper().ToCharArray();
+            char[] idarray = ID.ToCharArray();
 
             switch (idarray.Take(2).ToString())
             {
-                case ("FI"): return $"HTTPS://Posti.Fi/Seuranta/{id}";                                                
-                case ("MA"): return $"HTTPS://Matkahuolto.fi/Seuranta/{id}";    // These are not ready urls
-
-                default: throw new ArgumentException("Could not find the firm");                                     
+                case ("FI"):
+                    Company = "Posti";
+                    return $"HTTPS://Posti.Fi/Seuranta/{ID}";
+                case ("MA"):
+                    Company = "Matkahuolto";
+                    return $"HTTPS://Matkahuolto.fi/Seuranta/{ID}";                     // These are not ready urls
+                default:
+                    throw new ArgumentException("Could not find the firm");
             }
-
         }
+
         /// <summary>
         /// This is just a place holder to ensure that there are no immediate errors in the deserialize line.
         /// We will need to create probably an actual class that will be able to take the information from every company,
