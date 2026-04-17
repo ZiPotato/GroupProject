4090fecef1305cdc6951802665bf645c4a1e14dc
Vesa-Pekka Tasma <ah8515@student.jamk.fi>
2026-04-17 08:24:39 +0300

    Package class base started

--- Project/LähetysSeurantaConsole/Model/Package/Package.cs
+++ Project/LähetysSeurantaConsole/Model/Package/Package.cs
@@ -16,17 +16,17 @@ namespace LähetysSeurantaConsole.Model.Package
         {
             string url = IDHandling(ID);        // From here we seek to handle the data given to us by the url
             
+
         }
 
         private static string IDHandling(string id)
         {
             char[] idarray = id.ToCharArray();
-            string firmID = null;
-            if (char.IsLetter(idarray[0]) && char.IsLetter(idarray[1])) firmID = idarray.Take(2).ToString();
-
-            switch (firmID)
+            
+            switch (idarray.Take(2).ToString())
             {
                 case ("FI"): return $"HTTPS://Posti.Fi/Seuranta{id}";                                                // Here goes the firm url potentially with the already altered uri elements
+
                 default: throw new ArgumentException("Could not find the firm");                                     // We probably will need to create our own exception class that will inform the user precicely the way we want
             }
 
