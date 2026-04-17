f8609a7b1afadbb24226bd2e40049dfe83e38ddd
Aapeli Juujärvi <ah6911@student.jamk.fi>
2026-04-17 09:21:21 +0300

    Added a base for Order class

---
+++ Project/LähetysSeurantaConsole/Model/Order/Order.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LähetysSeurantaConsole.Model.Order
+{
+    internal class Order
+    {
+        public string TrackingID { get; private set; }
+        public string Carrier { get; private set; }
+
+        public Order(string trackingId, string carrier="")
+        {
+            TrackingID = trackingId;
+            Carrier = carrier;
+        }
+    }
+}
