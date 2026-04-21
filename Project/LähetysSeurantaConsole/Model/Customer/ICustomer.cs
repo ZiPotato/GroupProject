ae15d6f3b40f5652fbd8a97e4022b01cd40c6bd1
Aapeli Juujärvi <ah6911@student.jamk.fi>
2026-04-21 10:32:22 +0300

    Changed Id init to set

--- Project/LähetysSeurantaConsole/Model/Customer/ICustomer.cs
+++ Project/LähetysSeurantaConsole/Model/Customer/ICustomer.cs
@@ -7,7 +7,7 @@ namespace LähetysSeurantaConsole.Model.Customer
 {
     internal interface ICustomer
     {
-        public string Id { get; init; }
+        public string Id { get; set; }
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
         public string Name { get; set; }
