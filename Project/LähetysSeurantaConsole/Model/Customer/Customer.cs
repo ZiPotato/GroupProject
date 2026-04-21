dc76b6ea38108ffeb2516758bb6bce6b6d59c3b1
Aapeli Juujärvi <ah6911@student.jamk.fi>
2026-04-21 10:38:37 +0300

    Fix

--- Project/LähetysSeurantaConsole/Model/Customer/Customer.cs
+++ Project/LähetysSeurantaConsole/Model/Customer/Customer.cs
@@ -7,7 +7,7 @@ namespace LähetysSeurantaConsole.Model.Customer
         // Here we give the customer a cookie so they can be logged in everytime they enter the site
         // And ofcourse if we do want to look at the "give me your Email" way, I guess it will work.
 
-        public string Id { get; init; }
+        public string Id { get; set; }
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
         public string Name { get; set; }
