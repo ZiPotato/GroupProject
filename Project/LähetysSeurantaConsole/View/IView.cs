80518acee536808de22f8ae286a564c92bf4c9e9
Aapeli Juujärvi <ah6911@student.jamk.fi>
2026-04-21 10:42:03 +0300

    Added customer info properties to IView

--- Project/LähetysSeurantaConsole/View/IView.cs
+++ Project/LähetysSeurantaConsole/View/IView.cs
@@ -8,5 +8,10 @@ namespace LähetysSeurantaConsole.View
     {
         public string TrackingId { get; set; }
         public string Display { get; set; }
+
+        public string Id { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Name { get; set; }
     }
 }
