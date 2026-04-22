51a2bf9f7874e1f1fcf786468cb3dd6823691330
Aapeli Juujärvi <ah6911@student.jamk.fi>
2026-04-22 08:43:31 +0300

    Added two petentially temporary methods to IView

--- Project/LähetysSeurantaConsole/View/IView.cs
+++ Project/LähetysSeurantaConsole/View/IView.cs
@@ -13,5 +13,9 @@ namespace LähetysSeurantaConsole.View
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
         public string Name { get; set; }
+
+
+        public bool AddPackage();
+        public bool DisplayLatestPackage();
     }
 }
