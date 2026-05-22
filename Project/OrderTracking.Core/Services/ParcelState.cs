4269564b5ea4d2c9851e383947d60e7300e4ca6e
Aapo <ah6907@student.jamk.fi>
2026-05-22 08:46:21 +0300

    This is State for active and delivered parcels.

---
+++ Project/OrderTracking.Core/Services/ParcelState.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OrderTracking.Core.Models.Package;
+
+namespace OrderTracking.Core.Services
+{
+    public class ParcelState
+    {
+        public List<Parcel> ActiveParcels { get; set; } = new();
+        public List<Parcel> DeliveredParcels { get; set; } = new();
+
+
+    }
+}
